b292844dc31f42d8499b55988c7f02a4317b95ca
kamelot88 <vasilchenkoov1988@gmail.com>
2020-11-09 22:15:24 +0200

    fix: animation main block

--- js/control.js
+++ js/control.js
@@ -1,19 +1,18 @@
 /* Слайдер_начало */
 
-let firstElement = document.querySelector("body > header > div.navigation_header.main_flex.flex__jcontent_between");
-let elementLeft = document.querySelector("body > header > div.navigation_header.main_flex.flex__jcontent_between > div.navigation_left.main_flex_column_nw.flex__jcontent_between");
-let elementRight = document.querySelector("body > header > div.navigation_header.main_flex.flex__jcontent_between > div.navigation_right > nav");
-
+//дожидаемся полной загрузки страницы
 window.onload = function () {
-firstElement.onmouseout = function(e) {
-    // elementLeft.style.left = 0 + 'px';
-  };
-
+  //ищем элементы по селектору
+  var firstElement = document.querySelector("body > header > div.navigation_header.main_flex.flex__jcontent_between");
+  var elementLeft = document.querySelector("body > header > div.navigation_header.main_flex.flex__jcontent_between > div.navigation_left.main_flex_column_nw.flex__jcontent_between");
+  var elementRight = document.querySelector("body > header > div.navigation_header.main_flex.flex__jcontent_between > div.navigation_right");
+  //вешаем на него события
+  firstElement.onmouseout = function(e) {
+    elementLeft.style.left= -48 + '%';
+    elementRight.style.right= -30 + '%';
+  }
+  firstElement.onmouseover = function(e) {
+    elementLeft.style.left= 0 + '%';
+    elementRight.style.right= 0 + '%';
+  }
 }
-   
-
-
-
-
-  
-/* Слайдер_конец */
